1e1b2aae01383175cdac9b35e98cd42e35803dac
Nayeem Rahman <mayeem42@gmail.com>
2025-12-05 19:29:07 +0600

    Fix User model import case for Vercel

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import User from "@/models/user";
+import User from "@/models/User";
 import dbConnect from "@/libs/mongodb";
 import { compare } from "bcryptjs";
 
